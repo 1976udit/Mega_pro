cf58a66ef6c307533f91a35777d9c79e4a170d65
1976udit <165473837+1976udit@users.noreply.github.com>
2024-04-17 18:48:50 +0000

    new

---
+++ Blog_App/src/components/container/Container.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Container({childrens}){
+    return (
+        <div className='w-full max-w-7xl mx-auto px-4'>{childrens}</div>
+    )
+}
+
+export default Container;
